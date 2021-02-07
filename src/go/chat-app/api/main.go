d83bbc5bc52d4ef81f2043f31d350f21433cdef0
Stephane <stephstephg@gmail.com>
2021-02-07 20:37:35 +0100

    routing with gorilla mux

--- src/go/chat-app/api/main.go
+++ src/go/chat-app/api/main.go
@@ -5,10 +5,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 // Article represents an article for our API
 type Article struct {
+	ID      string `json:"ID"`
 	Title   string `json:"Title"`
 	Desc    string `json:"desc"`
 	Content string `json:"content"`
@@ -27,16 +30,32 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint hit : homepage")
 }
 
+func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
+	fmt.Fprintf(w, "Key: "+key)
+
+	for _, article := range Articles {
+		if article.ID == key {
+			json.NewEncoder(w).Encode(article)
+		}
+	}
+}
+
 func handleRequests() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/articles", returnAllArticles)
-	log.Fatal(http.ListenAndServe(":10000", nil))
+
+	myRouter := mux.NewRouter()
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/all", returnAllArticles)
+	myRouter.HandleFunc("/article/{id}", returnSingleArticle)
+	log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
 
 func main() {
+	fmt.Println("Rest API v2.0 - Mux Routers")
 	Articles = []Article{
-		{Title: "hello", Desc: "Article Description", Content: "Article content"},
-		{Title: "hello2", Desc: "Article 2 Description", Content: "Article 2 content"},
+		{ID: "1", Title: "hello", Desc: "Article Description", Content: "Article content"},
+		{ID: "2", Title: "hello2", Desc: "Article 2 Description", Content: "Article 2 content"},
 	}
 	handleRequests()
 }
